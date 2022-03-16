1f9688751a188b6a720e73fab9a739623ebd6e78
timokluser-dev <timo@timokluser.ch>
2022-03-16 14:48:35 +0100

    feat: add task 02

--- 02_input-validation/helpers.php
+++ 02_input-validation/helpers.php
@@ -1,11 +1,135 @@
 <?php
 
-class Helpers
+abstract class Helpers
 {
-    public static function debug($output)
+    /**
+     * static constructor
+     */
+    public static function init()
+    {
+        self::sanitizePostAndGet();
+    }
+
+    /**
+     * print out variable in debug
+     * @param mixed $output variable to print
+     */
+    public static function debug(mixed $output)
     {
         echo '<pre>';
-        print_r($output);
+
+        if (is_array($output) || is_object($output)) {
+            print_r($output);
+        } else {
+            var_dump($output);
+        }
+
         echo '</pre>';
     }
+
+    /**
+     * sanitize html input and prevent xss.
+     * **IMPORTANT:** POST and GET variables are already sanitized!
+     */
+    public static function sanitize(string $input)
+    {
+        return htmlspecialchars($input);
+    }
+
+    /**
+     * sanitizes global GET and POST variables
+     */
+    private static function sanitizePostAndGet()
+    {
+        global $_GET;
+        global $_POST;
+        $_GET = filter_input_array(INPUT_GET, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    }
+
+    public static function getFormField(string $key)
+    {
+        if (!$_POST || ($_POST && !array_key_exists($key, $_POST))) {
+            return null;
+        }
+
+        if (is_array($_POST[$key]) || is_object($_POST[$key])) {
+            return $_POST[$key];
+        }
+
+        return trim($_POST[$key]);
+    }
+
+    /**
+     * get a form field with long text.
+     * includes convert from nl to html <br />
+     * @return string form field text
+     */
+    public static function getFormFieldLongText(string $key)
+    {
+        return nl2br(self::getFormField($key));
+    }
+
+    /**
+     * get a json object
+     * @param string $path relative path
+     * @return object json object
+     */
+    public static function getJson(string $path)
+    {
+        $json = file_get_contents($path);
+        $object = json_decode($json);
+        return $object;
+    }
+
+    /**
+     * if is a form post back
+     */
+    public static function isPost()
+    {
+        return $_POST && count($_POST);
+    }
+
+    /**
+     * prevent a resubmit of the form with a JS approach
+     */
+    public static function preventReSubmit()
+    {
+
+        echo "
+        <script>
+            if (window.history.replaceState) {
+                window.history.replaceState(null, null, window.location.href);
+                console.log('cleared');
+            }
+        </script>
+        ";
+    }
+
+    /**
+     * check for valid email and get email parts
+     * @return bool|string[] `false` if no email - `string[]` with parts if valid email
+     * 
+     * `string[1]` => mailbox  
+     * `string[2]` => domain name  
+     * `string[3]` => tld  
+     */
+    public static function formatEmail(string $email)
+    {
+        // @see https://ihateregex.io/expr/email/
+        $emailPattern = '/([^@ \t\r\n]+)@([^@ \t\r\n]+)\.([^@ \t\r\n]+)/';
+
+        if (preg_match($emailPattern, $email, $matches)) {
+            return $matches;
+        } else {
+            return false;
+        }
+    }
+
+    public static function array_includes(string $str, array $array)
+    {
+        return array_search($str, $array) !== false;
+    }
 }
+
+Helpers::init();
--- 02_input-validation/index.php
+++ 02_input-validation/index.php
@@ -4,8 +4,57 @@
 <?php
 require_once './helpers.php';
 
-if (count($_POST)) {
-  Helpers::debug($_POST);
+$errors = [];
+
+if (Helpers::isPost()) {
+  // Helpers::preventReSubmit();
+
+  $anrede = Helpers::getFormField('Anrede');
+  if (!$anrede) {
+    array_push($errors, 'Keine Anrede angegeben');
+  }
+  $anreden = ['mr', 'ms'];
+  if (!Helpers::array_includes($anrede, $anreden)) {
+    array_push($errors, 'Keine gültige Anrede angegeben');
+  }
+
+  $vorname = Helpers::getFormField('Vorname');
+  if (!$vorname) {
+    array_push($errors, 'Keinen Vornamen angegeben');
+  }
+
+  $nachname = Helpers::getFormField('Nachname');
+  if (!$nachname) {
+    array_push($errors, 'Keinen Nachname angegeben');
+  }
+
+  $email = Helpers::getFormField('Email');
+  if (!$email) {
+    array_push($errors, 'Keine Email angegeben');
+  }
+
+  $promo = Helpers::getFormField('Promo');
+  $promoCodes = Helpers::getJson('promo.json');
+  if ($promo && Helpers::array_includes($promo, $promoCodes->promos) === false) {
+    array_push($errors, 'Ungültiger Promo-Code eingegeben');
+  }
+
+  $anzahl = Helpers::getFormField('Anzahl');
+  if (!$anzahl) {
+    array_push($errors, 'Keine Anzahl Tickets angegeben');
+  }
+
+  $sektion = Helpers::getFormField('Sektion');
+  if (!$sektion) {
+    array_push($errors, 'Keine Sektion(en) ausgewählt');
+  }
+
+  $kommentare = Helpers::getFormFieldLongText('Kommentare');
+
+  $agb = (bool) Helpers::getFormField('AGB');
+  if (!$agb) {
+    array_push($errors, 'Die AGBs wurden nicht akzeptionert');
+  }
 }
 ?>
 
@@ -17,7 +66,7 @@ if (count($_POST)) {
 
   <style>
     @charset "UTF-8";
-    
+
     form>div {
       margin-bottom: 10px;
     }
@@ -25,76 +74,177 @@ if (count($_POST)) {
     .mt {
       margin-top: 20px;
     }
+
+    .form-detail__container span {
+      font-weight: 600;
+    }
+
+    .form-detail__container>div>p,
+    .form-detail__container>div>span {
+      display: inline;
+    }
   </style>
 </head>
 
 <body>
   <h1>WM-Ticketservice</h1>
 
-  <form method="POST">
-    <div>
-      <input type="radio" name="anrede" id="herr" value="mr" required>
-      <label for="herr">Herr</label>
+  <?php
+  if (count($errors)) :
+  ?>
 
-      <input type="radio" name="anrede" id="frau" value="ms" required>
-      <label for="frau">Frau</label>
+    <div class="form-errors">
+      <h2>Folgende Fehler sind aufgetreten:</h2>
+
+      <ul>
+        <?php
+        foreach ($errors as $error) :
+        ?>
+          <li><?php echo $error; ?></li>
+        <?php
+        endforeach;
+        ?>
+      </ul>
     </div>
 
-    <div>
-      <label for="vorname">Vorname</label>
-      <input type="text" name="Vorname" id="vorname" required />
+  <?php
+  endif;
+
+  if (Helpers::isPost() && !$errors) :
+  ?>
+
+    <div class="form-detail">
+      <h2>Besten Dank!</h2>
+      <h3>Ihre Angaben:</h3>
+
+      <div class="mt form-detail__container">
+        <div>
+          <span>Anrede:</span>
+          <p><?php echo $anrede; ?></p>
+        </div>
+        <div>
+          <span>Vorname:</span>
+          <p><?php echo $vorname; ?></p>
+        </div>
+        <div>
+          <span>Nachname:</span>
+          <p><?php echo $nachname; ?></p>
+        </div>
+        <div>
+          <span>Email:</span>
+          <p><?php echo $email; ?></p>
+        </div>
+        <div>
+          <span>Promo:</span>
+          <p><?php echo $promo; ?></p>
+        </div>
+        <div>
+          <span>Anzahl Karten:</span>
+          <p><?php echo $anzahl; ?></p>
+        </div>
+        <div>
+          <span>Sektion:</span>
+          <p><?php echo join(' ', $sektion); ?></p>
+        </div>
+        <div>
+          <span>Kommentar:</span>
+          <div>
+            <p><?php echo ($kommentare) ? $kommentare : '-- keinen --'; ?></p>
+          </div>
+        </div>
+        <div>
+          <span>AGB:</span>
+          <p><?php echo ($agb) ? '✅' : ''; ?></p>
+        </div>
+      </div>
     </div>
 
-    <div>
-      <label for="nachname">Nachname</label>
-      <input type="text" name="Nachname" id="nachname" required />
+  <?php
+
+  else :
+  ?>
+
+    <div class="mt form">
+      <form method="POST">
+        <div>
+          <input type="radio" name="Anrede" id="herr" value="mr" <?php if (Helpers::getFormField('Anrede') === 'mr') : echo 'checked';
+                                                                  endif; ?> required>
+          <label for="herr">Herr</label>
+
+          <input type="radio" name="Anrede" id="frau" value="ms" <?php if (Helpers::getFormField('Anrede') === 'ms') : echo 'checked';
+                                                                  endif; ?> required>
+          <label for="frau">Frau</label>
+        </div>
+
+        <div>
+          <label for="vorname">Vorname</label>
+          <input type="text" name="Vorname" id="vorname" value="<?php echo Helpers::getFormField('Vorname'); ?>" required />
+        </div>
+
+        <div>
+          <label for="nachname">Nachname</label>
+          <input type="text" name="Nachname" id="nachname" value="<?php echo Helpers::getFormField('Nachname'); ?>" required />
+        </div>
+
+        <div>
+          <label for="email">E-Mail-Adresse </label>
+          <input type="email" name="Email" id="email" value="<?php echo Helpers::getFormField('Email'); ?>" required />
+        </div>
+
+        <div>
+          <label for="promo">Promo-Code</label>
+          <input type="password" name="Promo" id="promo" value="<?php echo Helpers::getFormField('Promo'); ?>" />
+        </div>
+
+        <div>
+          <label for="anzahl">Anzahl Karten</label>
+          <select name="Anzahl" id="anzahl" required>
+            <option value="" disabled <?php if (!Helpers::getFormField('Anzahl')) : echo 'selected';
+                                      endif; ?>>Bitte wählen</option>
+            <option value="1" <?php if (Helpers::getFormField('Anzahl') === '1') : echo 'selected';
+                              endif; ?>>1</option>
+            <option value="2" <?php if (Helpers::getFormField('Anrede') === '2') : echo 'selected';
+                              endif; ?>>2</option>
+            <option value="3" <?php if (Helpers::getFormField('Anrede') === '3') : echo 'selected';
+                              endif; ?>>3</option>
+            <option value="4" <?php if (Helpers::getFormField('Anrede') === '4') : echo 'selected';
+                              endif; ?>>4</option>
+          </select>
+        </div>
+
+        <div>
+          <label for="sektion">Gewünschte Sektion im Stadion</label>
+          <select name="Sektion[]" size="4" multiple="multiple" id="sektion" required>
+            <option value="nord" <?php if (Helpers::getFormField('Sektion') && Helpers::array_includes('nord', Helpers::getFormField('Sektion')) !== false) : echo 'selected';
+                                  endif; ?>>Nordkurve</option>
+            <option value="sued" <?php if (Helpers::getFormField('Sektion') && Helpers::array_includes('sued', Helpers::getFormField('Sektion')) !== false) : echo 'selected';
+                                  endif; ?>>Südkurve</option>
+            <option value="haupt" <?php if (Helpers::getFormField('Sektion') && Helpers::array_includes('haupt', Helpers::getFormField('Sektion')) !== false) : echo 'selected';
+                                  endif; ?>>Haupttribüne</option>
+            <option value="gegen" <?php if (Helpers::getFormField('Sektion') && Helpers::array_includes('gegen', Helpers::getFormField('Sektion')) !== false) : echo 'selected';
+                                  endif; ?>>Gegentribüne</option>
+          </select>
+        </div>
+
+        <div>
+          <label for="kommentare">Kommentare/Anmerkungen</label>
+          <textarea cols="70" rows="10" name="Kommentare" id="kommentare"><?php echo Helpers::getFormField('Kommentare'); ?></textarea>
+        </div>
+
+        <div class="mt">
+          <label for="agb">Ich akzeptiere die AGB.</label>
+          <input type="checkbox" name="AGB" id="agb" value="true" checked="<?php echo Helpers::getFormField('AGB'); ?>" required />
+        </div>
+
+        <div class="mt">
+          <button type="submit">Bestellung aufgeben</button>
+        </div>
+      </form>
     </div>
 
-    <div>
-      <label for="email">E-Mail-Adresse </label>
-      <input type="email" name="Email" id="email" required />
-    </div>
-
-    <div>
-      <label for="promo">Promo-Code</label>
-      <input type="password" name="Promo" id="promo" />
-    </div>
-
-    <div>
-      <label for="anzahl">Anzahl Karten</label>
-      <select name="Anzahl" id="anzahl" required>
-        <option value="" disabled selected>Bitte wählen</option>
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-      </select>
-    </div>
-
-    <div>
-      <label for="sektion">Gewünschte Sektion im Stadion</label>
-      <select name="Sektion[]" size="4" multiple="multiple" id="sektion" required>
-        <option value="nord">Nordkurve</option>
-        <option value="sued">Südkurve</option>
-        <option value="haupt">Haupttribüne</option>
-        <option value="gegen">Gegentribüne</option>
-      </select>
-    </div>
-
-    <div>
-      <label for="kommentare">Kommentare/Anmerkungen</label>
-      <textarea cols="70" rows="10" name="Kommentare" id="kommentare"></textarea>
-    </div>
-
-    <div class="mt">
-      <label for="agb">Ich akzeptiere die AGB.</label>
-      <input type="checkbox" name="AGB" id="agb" value="ok" required />
-    </div>
-
-    <div class="mt">
-      <button type="submit">Bestellung aufgeben</button>
-    </div>
-  </form>
+  <?php
+  endif;
+  ?>
 </body>
 
 </html>
